78ad24a985a64a0d0d1044053b98cafe7cdc5b3a
davi38 <davi4alves@gmail.com>
2022-10-04 18:43:36 -0300

    feat: front end dashboard start

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ToastContainer } from 'react-toastify'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+const isLoggedIn = true
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="container">
+        <BrowserRouter>
+          {isLoggedIn ? (
+            <>
+              <header>
+                <Header />
+              </header>
+              <main>
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
+                </Routes>
+              </main>
+            </>
+          ) : (
+            <Login />
+          )}
+        </BrowserRouter>
+        <ToastContainer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import {
+  AppBar,
+  Box,
+  Button,
+  Toolbar,
+  Typography,
+  Tabs,
+  Tab,
+  Drawer,
+  List,
+  Divider,
+} from '@mui/material'
+import { Link } from 'react-router-dom'
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { mainListItems, secondaryListItems } from '../components/listItems';
+
+const Header = () => {
+  const [value, setValue] = useState(0)
+  const dispatch = useDispatch()
+  /* const isLoggedIn = useSelector(state => state.isLoggedIn) */
+  const isLoggedIn = true;
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+  return (
+
+    <AppBar sx={{
+      background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(98,98,255,1) 0%, rgba(0,212,255,1) 100%)',
+    }}>
+      <Toolbar>
+        {isLoggedIn && <Box display="flex" marginLeft="auto">
+          <Tabs
+            textColor="inherit"
+            value={value} onChange={(e, val) => setValue(val)}>
+            <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+            <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+            <Tab LinkComponent={Link} to="/blogs/add" label="Add Blog" />
+          </Tabs>
+        </Box>}
+        <Box display="flex" marginLeft="auto">
+          {!isLoggedIn && <> <Button
+            LinkComponent={Link} to="/login"
+            variant='contained'
+            sx={{ margin: 1, borderRadius: 10 }}
+            color='warning'>Login
+          </Button>
+            {/* <Button
+              LinkComponent={Link} to="/register"
+              variant='contained'
+              sx={{ margin: 1, borderRadius: 10 }}
+              color='warning'>Signup
+            </Button> */}
+          </>}
+          {isLoggedIn && <Button
+            onClick={() => dispatch(logout())}
+            LinkComponent={Link} to="/logout"
+            variant='contained'
+            sx={{ margin: 1, borderRadius: 10 }}
+            color='warning'>Logout
+          </Button>}
+        </Box>
+      </Toolbar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            px: [1],
+          }}
+        >
+          <Typography variant='h4'>Dashboard</Typography>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          {mainListItems}
+          <Divider sx={{ my: 1 }} />
+          {secondaryListItems}
+        </List>
+      </Drawer>
+    </AppBar>
+  )
+}
+
+export default Header
